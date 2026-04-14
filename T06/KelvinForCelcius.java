966d773690e06eb5c67ab4f89b6a50a2e0c07b86
GabrielDevUSJT <gabrielricca.tech@gmail.com>
2026-04-14 10:23:47 -0300

    Atividades dia 07/04

---
+++ T06/KelvinForCelcius.java
@@ -0,0 +1,33 @@
+package T06;
+
+
+public class KelvinForCelcius {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor inicial em Kelvin: ");
+        double inicio = sc.nextDouble();
+
+        System.out.print("Digite o valor final em Kelvin: ");
+        double fim = sc.nextDouble();
+
+        System.out.print("Digite o passo: ");
+        double passo = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Kelvin\t\tCelsius");
+        System.out.println("------------------------");
+
+        double atual = inicio;
+
+        while (atual <= fim) {
+            double celsius = atual - 273.15;
+            System.out.printf("%.2f\t\t%.2f%n", atual, celsius);
+            atual = atual + passo;
+        }
+
+        sc.close();
+    }
+}
---
+++ T06/Senhas.java
@@ -0,0 +1,32 @@
+package T06;
+
+import java.util.Scanner;
+
+public class Senhas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int SENHA_CORRETA = 2026;
+        int tentativas = 3;
+        boolean acessoPermitido = false;
+
+        while (tentativas > 0) {
+            System.out.print("Digite a senha numérica: ");
+            int senhaDigitada = sc.nextInt();
+
+            if (senhaDigitada == SENHA_CORRETA) {
+                acessoPermitido = true;
+                break;
+            } else {
+                tentativas--;
+                System.out.println("Senha incorreta. Tentativas restantes: " + tentativas);
+            }
+        }
+
+        if (acessoPermitido) {
+            System.out.println("Acesso Permitido!");
+        } else {
+            System.out.println("Terminal Bloqueado!");
+        }
+        sc.close();
+    }
+}
